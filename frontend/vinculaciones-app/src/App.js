53cb8b6899afeb508cadfd143f9e7da98b62dab0
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2022-10-11 00:45:26 -0300

    Creando App y agregando libreria React Bootstrap

---
+++ frontend/vinculaciones-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import {Button} from 'react-bootstrap';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <Button>Boton</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
