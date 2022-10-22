8029575edc1f7cc15041589624bd4dba8c23c1fb
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2022-10-22 15:18:27 -0300

    1

--- frontend/vinculaciones-app/src/App.js
+++ frontend/vinculaciones-app/src/App.js
@@ -1,13 +1,13 @@
 
 import './media/style/personalizado.css'
 
-import  NavBav from './media/components/NavBar/NavBar';
-import  Principal from './media/components/Inicio/Principal'
-import  Noticias from './media/components/Noticias/Noticias'
+import  NavBav from './media/components/ComponentsHome/NavBar/NavBar';
+import  Principal from './media/components/ComponentsHome/Inicio/Principal'
+import  Noticias from './media/components/ComponentsHome/Noticias/Noticias'
 import Gestion from './media/components/ComponentsHome/Gestion/Gestion'
 import Convocatorias from './media/components/ComponentsHome/Convocatorias/Convocatorias'
 import Redes from './media/components/Redes/Redes';
-import Vinculaciones from './media/components/Vinculaciones/Vinculaciones';
+import Vinculaciones from './media/components/ComponentsHome/Vinculaciones/Vinculaciones';
 import Consulta from './media/components/ComponentsHome/Consulta/Consulta';
 
 
