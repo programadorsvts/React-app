6bd2594c39a096c43aab9d3e7ac3029233fbfc97
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2022-10-22 16:40:32 -0300

    1

--- frontend/vinculaciones-app/src/App.js
+++ frontend/vinculaciones-app/src/App.js
@@ -2,11 +2,11 @@
 import './media/style/personalizado.css'
 
 import  NavBav from './media/components/ComponentsHome/NavBar/NavBar';
-import  Principal from './media/components/ComponentsHome/Inicio/Principal'
+import  Principal from './media/components/ComponentsHome/Principal/Principal'
 import  Noticias from './media/components/ComponentsHome/Noticias/Noticias'
 import Gestion from './media/components/ComponentsHome/Gestion/Gestion'
 import Convocatorias from './media/components/ComponentsHome/Convocatorias/Convocatorias'
-import Redes from './media/components/Redes/Redes';
+import Redes from './media/components/ComponentsHome/Redes/Redes';
 import Vinculaciones from './media/components/ComponentsHome/Vinculaciones/Vinculaciones';
 import Consulta from './media/components/ComponentsHome/Consulta/Consulta';
 
--- frontend/vinculaciones-app/src/media/components/ComponentsHome/NavBar/NavBar.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsHome/NavBar/NavBar.js
@@ -1,50 +1,3 @@
-/* import { Navbar, Container, Nav,Button  } from 'react-bootstrap';   */
- 
-
-
- /* function  NavBar() {  
-  return (  
-    <>  
-      <Navbar collapseOnSelect expand="lg"  bg="primary" >  
-        <Container>  
-          <Navbar.Brand href="" className="ms-0">
-            <Navbar.Brand href="">
-              
-                <img src="'../../image/icons/escudounslweb.png" width="30" height="30" className="d-inline-block align-top" alt=""/>
-                <img src="'../../image/icons/escudounslweb.png" width="30" height="30" className="d-inline-block align-top ms-2" alt=""/>
-          </Navbar.Brand>
-                 
-            </Navbar.Brand>  
-          <Navbar.Toggle  aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse  id="responsive-navbar-nav">  
-            
-            <Nav className="me-auto ">  
-                <Nav.Link href="#features" className="text-light">INICIO</Nav.Link>
-                <Nav.Link href="#features" className="text-light">CATALOGO</Nav.Link>  
-                <Nav.Link href="#pricing" className="text-light">REVISTA DIGITRAL</Nav.Link>  
-            </Nav>  
-           
-            <Nav >  
-
-              <Button className="bg-secondary text-dark ms-1 mt-1 ms-2">registrarse</Button>
-              <Button className="bg-secondary text-dark ms-1 mt-1 ms-2">iniciar sesion</Button>
-           
-
-            </Nav>  
-          </Navbar.Collapse>  
-          
-        
-        </Container>  
-      </Navbar>  
-    </>  
-  );  
-}   
-
-
-
-export default NavBar;   */
-
-
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -80,7 +33,7 @@ function NavBar() {
                   
                   
                 <Nav.Link href="#features" className="text-light">INICIO</Nav.Link>
-                <Nav.Link href="#features" className="text-light">CATALOGO</Nav.Link>  
+                
                 <Nav.Link href="#pricing" className="text-light">REVISTA DIGITRAL</Nav.Link>
                 <Nav.Link href="#pricing" className="text-light">OBSERVATORIO</Nav.Link>   
                 
