1f485004e2b6d3bec80c59abddbd6e45650a6713
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2022-10-23 20:54:39 -0300

    1

--- frontend/vinculaciones-app/src/App.js
+++ frontend/vinculaciones-app/src/App.js
@@ -1,43 +1,22 @@
 
 import './media/style/personalizado.css' 
 
-import  NavBav from './media/components/ComponentsHome/NavBar/NavBar';
-import  Principal from './media/components/ComponentsHome/Principal/Principal'
-import  Noticias from './media/components/ComponentsHome/Noticias/Noticias'
-import Gestion from './media/components/ComponentsHome/Gestion/Gestion'
-import Convocatorias from './media/components/ComponentsHome/Convocatorias/Convocatorias'
-import Redes from './media/components/ComponentsHome/Redes/Redes';
-import Vinculaciones from './media/components/ComponentsHome/Vinculaciones/Vinculaciones';
-import Consulta from './media/components/ComponentsHome/Consulta/Consulta';
-import Footer from './media/components/ComponentsHome/Footer/Footer';
-
-
+import { BrowserRouter as Router, Switch,Route,Link } from "react-router-dom";
+import { Suspense } from 'react';
 
 function App() {
+
   return (
-    <div className="App bg-warning"  >
-     
+   
+     <Router>
+
+      <Suspense fallback={<p>.............Loading</p>}>
+
+      </Suspense>
+
+     </Router>
       
-           
-       
-
-              <NavBav ></NavBav>
-              <Principal></Principal>
-              <Noticias></Noticias>
-              <Gestion></Gestion>
-              <Convocatorias></Convocatorias>
-              <Vinculaciones></Vinculaciones>
-              <Redes></Redes>
-              <Consulta></Consulta>
-              <Footer></Footer>
           
-
-          
-          
-         
-      
-
-    </div>
   );
 }
 
