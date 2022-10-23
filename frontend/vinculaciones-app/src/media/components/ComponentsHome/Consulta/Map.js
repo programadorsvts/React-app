5d722436189aac3226b019cb658a6acfda97caf7
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2022-10-22 21:41:59 -0300

    1

--- frontend/vinculaciones-app/src/media/components/ComponentsHome/Consulta/Map.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsHome/Consulta/Map.js
@@ -1,21 +1,21 @@
 
 import  {MapContainer, TileLayer, Marker,Popup } from 'react-leaflet'
-/* import 'react-leaflet/lib/' */
+import "leaflet/dist/leaflet.css";
 
  const map=()=>{
     return(
 
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-            </MapContainer>
+       <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+                <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
             );
  }
 
