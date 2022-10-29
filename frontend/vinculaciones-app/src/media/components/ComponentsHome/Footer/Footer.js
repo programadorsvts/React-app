1bed9f3e6abc698c073e0f64f4f36c392a481c81
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2022-10-29 18:35:48 -0300

    Corrigiendo las diferencias entre las etiquetas HTML y los componentes Router Dom
    🥵​🥵​🥵​

--- frontend/vinculaciones-app/src/media/components/ComponentsHome/Footer/Footer.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsHome/Footer/Footer.js
@@ -13,28 +13,27 @@ function footer() {
             <Col className=" text-center mt-5" xs={12} md={4}>
                   
                 <h6 className="text-uppercase ">Información Institucional</h6>
-                <mg className="" src="'../../image/icons/escudounslweb.png" width="150" height="100" alt=""/>  
+                <img src="../../image/icons/escudounslweb.png" width="150" height="100" alt=""/>  
                 
-                <ul className="list-unstyled  ">
-                    <li className="">Dirección: <Link className='text-secondary' to="https://www.google.com/maps/place/Universidad+Nacional+de+San+Luis/@-33.2920509,-66.341365,17.6z/data=!4m9!1m2!2m1!1sunsl!3m5!1s0x95d4394eec782487:0x2bcfdfba83514ad4!8m2!3d-33.2921332!4d-66.3396195!15sCgR1bnNsWgYiBHVuc2ySAQp1bml2ZXJzaXR5mgEkQ2hkRFNVaE5NRzluUzBWSlEwRm5TVVJWT1daNk5ucFJSUkFC4AEA" Target="_blank">Ejército de los Andes 950 San Luis</Link></li>
-                    <p className="">San Luis Argentina</p>
-                    <li className="">Telefono: <Link className="text-secondary" to="tel:+54 (0266) 4520300">+54 (0266) 4520300</Link></li>
+                <ul className="list-unstyled">
+                    <li >Dirección: <Link className="text-secondary" to="https://www.google.com/maps/place/Universidad+Nacional+de+San+Luis/@-33.2920509,-66.341365,17.6z/data=!4m9!1m2!2m1!1sunsl!3m5!1s0x95d4394eec782487:0x2bcfdfba83514ad4!8m2!3d-33.2921332!4d-66.3396195!15sCgR1bnNsWgYiBHVuc2ySAQp1bml2ZXJzaXR5mgEkQ2hkRFNVaE5NRzluUzBWSlEwRm5TVVJWT1daNk5ucFJSUkFC4AEA" target="_blank" rel="noopener noreferrer">Ejército de los Andes 950 San Luis</Link></li>
+                    <p >San Luis Argentina</p>
+                    <li >Telefono: <Link className="text-secondary" to="tel:+54 (0266) 4520300">+54 (0266) 4520300</Link></li>
                 </ul>
 
             </Col>
           
-            <Col className=" text-center mt-5"xs={12} md={4}>
+            <Col className="text-center mt-5"xs={12} md={4}>
                   
                 <h6 className="text-uppercase">Contacto</h6>
-                <img className="" src="'../../image/icons/escudounslweb.png" width="150" height="100" alt=""/>  
+                <img src="../../image/icons/escudounslweb.png" width="150" height="100" alt=""/>  
 
                 <ul className="list-unstyled  ">
                     
-                    <li  className="">Ubicacion: <Link  className="text-secondary"   to="https://www.google.com/maps/search/unsl/@-33.2918874,-66.3401793,19.96z" target="_blank">1er Piso - Ala B</Link></li>
-                    <p className="">San Luis Argentina</p>
-                    <li className="">Telefono: <Link  className="text-secondary"  to="tel:+54 (0266) 4520300">+54 (0266) 4520300</Link></li>
-                    <li className="">Telefono: <Link  className="text-secondary" to="tel:5159">5159</Link></li>
-                    <li className="">Email: <Link  className="text-secondary" to="mailto: svts.unsl@gmail.com">svts.unsl@gmail.com</Link></li>
+                    <p >San Luis Argentina</p>
+                    <li >Telefono: <Link  className="text-secondary"  to="tel:+54 (0266) 4520300">+54 (0266) 4520300</Link></li>
+                    <li >Telefono: <Link  className="text-secondary" to="tel:5159">5159</Link></li>
+                    <li >Email: <Link  className="text-secondary" to="mailto: svts.unsl@gmail.com">svts.unsl@gmail.com</Link></li>
                 </ul>
 
             </Col>
@@ -52,7 +51,7 @@ function footer() {
         </Row>
         <Row  className="text-center text-secondary bg-primary">
                
-                <p className=" mt-4">&#169; COPYRIGHT 2021 Secretaría de Vinculación Tecnológica y Social (UNSL) <br />Diseñado y desarrollado por Octavio Riccardo y Iván Agustín Lucero</p>
+                <p className="mt-4">&#169; COPYRIGHT 2021 Secretaría de Vinculación Tecnológica y Social (UNSL) <br />Diseñado y desarrollado por Octavio Riccardo y Iván Agustín Lucero</p>
 
                 
 
