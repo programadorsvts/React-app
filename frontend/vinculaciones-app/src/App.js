a9e28eb2f9bde07b375795f5e721a69d48916a1f
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2022-10-29 14:03:50 -0300

    1

--- frontend/vinculaciones-app/src/App.js
+++ frontend/vinculaciones-app/src/App.js
@@ -1,13 +1,15 @@
- import { BrowserRouter as Router ,/* Route */ } from "react-router-dom"; 
+ ///////////React Router Dom////////////////////
+ import { BrowserRouter as Router ,Route,Routes  } from "react-router-dom"; 
 
+/////////////////Pages/////////////////////////
+import observatorio from './media/pages/observatorio';
+import revistadigital from './media/pages/revistadigital' 
+import home from './media/pages/home' 
 
-/* import observatorio from './media/pages/observatorio';
-  import revistadigital from './media/pages/revistadigital' */
-  /* import home from './media/pages/home' */
 import NavBar from "./media/components/ComponentsHome/NavBar/NavBar"
 import Principal from "./media/components/ComponentsHome/Principal/Principal";
 
-
+/////////////////Bootstrap/////////////////
  import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 
@@ -17,10 +19,15 @@ function App() {
   return (
    
                 <Router> 
-                 
-                  <NavBar></NavBar>
-                  <Principal></Principal>
-               
+                      <Routes>
+
+                             <Route exact path="/" element={home} /> 
+
+                              <Route exact path="/revistadigital" element={revistadigital} />
+
+                              <Route exact path="/observatorio" element={observatorio} />
+                              
+                      </Routes>       
                 </Router>   
   );
 }
