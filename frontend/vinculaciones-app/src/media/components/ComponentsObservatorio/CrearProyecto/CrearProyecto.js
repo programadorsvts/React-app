6aea71a51035ceaf82e5f0dab39a692ca2acdee3
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2022-10-30 18:09:06 -0300

    1

--- frontend/vinculaciones-app/src/media/components/ComponentsObservatorio/CrearProyecto/CrearProyecto.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsObservatorio/CrearProyecto/CrearProyecto.js
@@ -1,13 +1,14 @@
 import './crearproyecto.css'
 
 
+
 import {Button,Container} from 'react-bootstrap';
 
 function crearproyecto() {
   return (
 
-    <Container className="d-grid gap-2 mt-2">
-      <Button className='crear-proyectos 'size="lg">Crear Proyecto</Button>
+    <Container className="d-grid gap-2 mt-2 crear-proyectos   ">
+      <Button className=' text btn  'size="lg">Crear Proyecto</Button>
  
     </Container>
   );
