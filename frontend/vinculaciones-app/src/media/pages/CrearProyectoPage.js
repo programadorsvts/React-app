d0d209551da939e0d0a6622cc15b01d75420c7ff
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2022-10-31 17:34:12 -0300

    Creando la pagina y los componentes de crear proyectos

--- frontend/vinculaciones-app/src/media/pages/CrearProyectoPage.js
+++ frontend/vinculaciones-app/src/media/pages/CrearProyectoPage.js
@@ -1,8 +1,12 @@
+/* import CrearPrincipal from "../components/ComponentsCrearProyecto/PrincipalCrearProyecto/Principal"; */
+import FormularioCrear from "../components/ComponentsCrearProyecto/FormularioCrearProyecto/FormularioCrear";
 
 function crearproyectopage(){
     return(
             <>
-            
+                    
+                    {/* <CrearPrincipal></CrearPrincipal> */}
+                    <FormularioCrear></FormularioCrear>
                
             </>
     );
