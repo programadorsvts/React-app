42f25e587e797476c7e2f40db98a2dbc04d15ccf
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2022-11-02 00:11:17 -0300

    1

--- frontend/vinculaciones-app/src/media/components/ComponentsHome/Consulta/Formulario.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsHome/Consulta/Formulario.js
@@ -1,35 +1,32 @@
 import {Button,Form} from 'react-bootstrap';
+import '../../../styles/form.css';
 
 
 
 function formulario() {
   return (
-    <Form className="m-2">
+    <Form className="  ">
 
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Group className="" controlId="exampleForm.ControlInput1">
         
             <Form.Label className="ms-2">Nombre y apellido</Form.Label>
                 <Form.Control type="text" placeholder=""  />
             <br />
 
-            <Form.Label className="ms-2">Email </Form.Label>
+            <Form.Label className="">Email </Form.Label>
             <Form.Control type="email" placeholder="ejemplo@unsl.com" />
             <br />
         
-            <Form.Label className="ms-2">Telefono</Form.Label>
+            <Form.Label className="">Telefono</Form.Label>
             <Form.Control type="text" placeholder="+54 ....." />
             <br />
-        
-      
         </Form.Group>
 
-            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Group className="" controlId="exampleForm.ControlTextarea1">
             <Form.Label className="ms-4">Descripcion</Form.Label>
-            <Form.Control as="textarea" rows={9} />
-            
-            
+            <Form.Control as="textarea" rows={9} />  
         </Form.Group>
-        <Button className="ms-3 mt-2 bg-secondary text-primary">Enviar</Button>
+        <Button className=''>Enviar</Button>
 
     </Form>
   
