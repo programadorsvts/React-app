8174ab9f177d54fae944b75c2b5ee0c29423d45c
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2022-11-14 21:43:24 -0300

    1

--- frontend/vinculaciones-app/src/api/Login.js
+++ frontend/vinculaciones-app/src/api/Login.js
@@ -1,33 +1,22 @@
 import {useRef, useState, useEffects } from '';
-import { ErrorMessage } from "formik";
+
 import { useEffect } from "react";
 
+import { create } from 'apisauce'
+
+
+const api = create({ baseURL: '        '})
 
 
 function Login =()=>{
 
-    const useRef = useRef();
-    const erroRef= useRef();
+  
 
-    const [ user,setUser]=useState('');
-    const [password,setPassword]=useState('');
-    const  [ErrorMessage, setErrorMessage]=useState('');
-    const [succes,setSucces]=useState(false);
-
-    useEffect(()=>{
-        
-
-
-    },[])
-
-    
-
-    
     return(
+    <>
 
 
 
-
-
+    </>
     )
 }
