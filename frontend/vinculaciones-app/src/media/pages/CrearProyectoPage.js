f3570f971a7af716a71a11ed4995ae5e131067a0
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2022-11-16 01:20:09 -0300

    Revert "1"
    This reverts commit 7a90feeaa9ee2d876771066bdc12e396cd6c757d.

--- frontend/vinculaciones-app/src/media/pages/CrearProyectoPage.js
+++ frontend/vinculaciones-app/src/media/pages/CrearProyectoPage.js
@@ -1,5 +1,5 @@
 /* import CrearPrincipal from "../components/ComponentsCrearProyecto/PrincipalCrearProyecto/Principal"; */
-import FormularioCrear from "../components/ComponentsCrear/FormularioCrear/FormularioCrear";
+import FormularioCrear from "../components/ComponentsCrearProyecto/FormularioCrear/FormularioCrear";
 
 function crearproyectopage(){
     return(
