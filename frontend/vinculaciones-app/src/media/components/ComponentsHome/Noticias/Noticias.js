2f1a8aa99c6d6aae74b35134a78a04eadaecc6e4
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2022-11-17 03:13:44 -0300

    herramientas page

--- frontend/vinculaciones-app/src/media/components/ComponentsHome/Noticias/Noticias.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsHome/Noticias/Noticias.js
@@ -12,7 +12,7 @@ function noticias() {
         const noticia1={imagen:'assets/images/noticia-1.jpg'  ,fecha:'4 de Junio de 2022',          texto:'Día de la Vinculación Tecnológica'};
         const noticia2={imagen:'assets/images/noticia-2.png' ,fecha:'5 de Octubre de 2022',     texto:'Día de la Vinculación Tecnológica' };
         const noticia3={imagen:'assets/images/noticia-3.png' ,fecha:'6 de Junio de 2022',          texto:'Día de la Vinculación Tecnológica' };
-        const noticia4={imagen:'assets/images/noticia-4.png' ,fecha:'7 de junio de 2022',  texto:'Día de la Vinculación Tecnológica' };
+        const noticia4={imagen:'assets/images/noticia-4.png' ,fecha:'7 de junio de 2022',          texto:'Día de la Vinculación Tecnológica' };
 
         
   return (
---
+++ frontend/vinculaciones-app/src/media/pages/HerramientasPage.js
@@ -0,0 +1,14 @@
+import Herramientas from "../components/ComponentsHerramientas/Herramientas/Herramientas";
+
+function herramientaspage(){
+    return(
+            <>
+                    
+                    <Herramientas></Herramientas>
+               
+            </>
+    );
+}
+
+
+ export default herramientaspage
