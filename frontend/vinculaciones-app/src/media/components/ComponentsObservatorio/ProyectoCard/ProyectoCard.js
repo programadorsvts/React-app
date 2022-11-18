dbff9477842dfa7b2e79f325b1b343d34db35230
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2022-11-17 23:36:05 -0300

    dandole estilo al modal en crear proyectos

--- frontend/vinculaciones-app/src/media/components/ComponentsObservatorio/ProyectoCard/ProyectoCard.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsObservatorio/ProyectoCard/ProyectoCard.js
@@ -2,16 +2,16 @@
 import {Card} from 'react-bootstrap'
 import './card.css';
 import {useState} from 'react';
+
 import ProyectoModal from '../ProyectoCard/ProyectoModal'
 
 
 function ProyectoCard(props) {
-
-      const [modalShow, setModalShow] =useState(false);
+      const [modalShow, setModalShow] = useState(false);
 
   return (
       <>
-              <Card className='card'  onClick={() => setModalShow(true)}>
+              <Card className='card' onClick={() => setModalShow(true)}>
                 <Card.Body>
                   <Card.Text className='encabezado-4'>
                     {props.titulo}
@@ -19,10 +19,8 @@ function ProyectoCard(props) {
                   <Card.Title className='text-1 text-deg text-capitalize'>{props.area}</Card.Title>
                 </Card.Body>
               </Card>
-                <ProyectoModal
-                          show={modalShow}
-                          onHide={() => setModalShow(false)}
-                  />
+                
+                <ProyectoModal show={modalShow} onHide={() => setModalShow(false)}/>
         </>       
   );
 }
--- frontend/vinculaciones-app/src/media/components/ComponentsObservatorio/ProyectoCard/ProyectoModal.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsObservatorio/ProyectoCard/ProyectoModal.js
@@ -1,21 +1,29 @@
 
-import {Modal} from 'react-bootstrap';
+import {Modal,CloseButton } from 'react-bootstrap';
 import './modal.css';
 
-function modal(props) {
-
+function ProyectoModal(props) {
   return (
         <>
-          <Modal {...props} className='modal modal-90w'  >
+            <Modal {...props} size="lg"  aria-labelledby="contained-modal-title-vcenter" centered >
+               
+                
+                <Modal.Body className='modal-body'>
+                    <CloseButton />
+                    <p  className=' text-deg'>Salud</p>                    
+                    <h4 className='encabezado-2 mt-3'>Jabón en comprimidos</h4>
+                    <p  className='encabezado-4'>Director/a :</p>
+                    <p className='encabezado-4'>Organizacion :</p>
+                    <p className='text-1' >Contacto:</p>
+                    <p className='text-2' >mail</p>
+                    <p className='text-2' >2664006580</p>
+                    <p className='text-2' >Direccion calle 3 </p>
+                    <p className='encabezado-2' >Descripcion: </p>
+                </Modal.Body>
 
-            <Modal.Body    className='modal-body' >
-    
-            
-            </Modal.Body>
-          </Modal>
+              </Modal>
       </>  
-
   );
 }
 
-export default modal;
+export default ProyectoModal;
