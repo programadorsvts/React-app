57aa61c1d0e7fc7663bafb94cad76929f8078c68
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2022-11-21 18:19:56 -0300

    Update Card.js

--- frontend/vinculaciones-app/src/media/components/ComponentsHome/Noticias/Card.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsHome/Noticias/Card.js
@@ -6,7 +6,7 @@ function card(props) {
   return (
 
     <Card className="card-noticias" style={{ width: '16rem' }}>
-      <a href='#'>
+      <a href='#0'>
         <Card.Img variant="top" src={props.info.imagen} />
         <Card.Body> 
           <Card.Title className='text-date'>{props.info.fecha}</Card.Title>
