a4ebe74535fe06f4d99597b3be950564374f9c65
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2022-11-22 02:34:15 -0300

    corrigiendo las rutas del github pages por 5ta vez

--- frontend/vinculaciones-app/src/media/components/ComponentsRevistaDigital/RevistaDigital.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsRevistaDigital/RevistaDigital.js
@@ -4,7 +4,7 @@ import RevistaCard from '../ComponentsRevistaDigital/RevistaCard';
 function revista(){
         const revista1=
         { 
-                imagen:'assets/revistadigital/revista1.jpg',   
+                imagen:'Vinculaciones-Frontend/assets/revistadigital/revista1.jpg',   
                 link:'https://svts.unsl.edu.ar/pdf/UNSL2.0-Jun20.pdf ',                                   
                 titulo:'Volumen 1 - Numero 1 ',    
                 fecha:'Junio 2020 ' , 
@@ -12,7 +12,7 @@ function revista(){
         }
         const revista2=
         { 
-                imagen:'assets/revistadigital/revista2.jpg',   
+                imagen:'Vinculaciones-Frontend/assets/revistadigital/revista2.jpg',   
                 link:'https://svts.unsl.edu.ar/pdf/Sept20-Rev.N2-iUNSL2.0-3.pdf ',                 
                 titulo:'Volumen 1 - Numero 2',      
                 fecha:'septiembre 2020' , 
@@ -20,7 +20,7 @@ function revista(){
         }
         const revista3=
         { 
-                imagen:'assets/revistadigital/revista3.jpg',   
+                imagen:'Vinculaciones-Frontend/assets/revistadigital/revista3.jpg',   
                 link:'https://svts.unsl.edu.ar/pdf/iunsl2.0-No3.-.pdf',                                      
                 titulo:'Volumen 1 - Numero 3',      
                 fecha:'diciembre 2020 ' , 
@@ -28,7 +28,7 @@ function revista(){
         }
         const revista4=
         { 
-                imagen:'assets/revistadigital/revista4.jpg',   
+                imagen:'Vinculaciones-Frontend/assets/revistadigital/revista4.jpg',   
                 link:'https://svts.unsl.edu.ar/pdf/iUNSL2.0%20No4.%20-comprimido.pdf',    
                 titulo:'Volumen 2 - Numero 4',      
                 fecha:'Junio 2021 ' , 
@@ -36,7 +36,7 @@ function revista(){
         }
         const revista5=
         {  
-                imagen:'assets/revistadigital/revista5.jpg',  
+                imagen:'Vinculaciones-Frontend/assets/revistadigital/revista5.jpg',  
                 link:'https://svts.unsl.edu.ar/pdf/Iunsl%202.0%20No5.pdf',                            
                 titulo:'Volumen 2 - Numero 5' ,    
                 fecha:'Diciembre 2021 ' , 
