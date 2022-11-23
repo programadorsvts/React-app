f321fd3f694a9d174d7fc236693bfbc4948f6e3e
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2022-11-23 00:10:41 -0300

    Lograr que el menu OffCanvas  cierre al usar los elementos Links
    usando el useRef , tendria que solucionar que no se vuelva a renderizar el DOM cuando se usa el NavBar normalmente

--- frontend/vinculaciones-app/src/media/components/ComponentsHome/NavBar/NavBar.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsHome/NavBar/NavBar.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
+import {useRef} from 'react';
 
 import {NavLink,useNavigate } from "react-router-dom";
 import Offcanvas from 'react-bootstrap/Offcanvas';
@@ -12,9 +13,15 @@ import './navbar.css';
 
 function NavBar() {
   const navigate = useNavigate();
+  const offCanvasRef = useRef();
+  
+  const closeOffCanvas = () => offCanvasRef.current.backdrop.click() ;
+
+
   return (
     <>
       {['lg' ].map((expand) => (
+        
         <Navbar key={expand} expand={expand} className="navbar" sticky="top">
           <Container fluid bg="primary">
             <Navbar.Brand href="/Vinculaciones-Frontend/">
@@ -22,21 +29,23 @@ function NavBar() {
               
             </Navbar.Brand>
             <Navbar.Toggle className='navbar-toggler' aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Offcanvas className='menu-toggle' id={`offcanvasNavbar-expand-${expand}`} aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`} placement="end">
+            <Navbar.Offcanvas className='menu-toggle' id={`offcanvasNavbar-expand-${expand}`} aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`} placement="end" ref={offCanvasRef} >
               <Offcanvas.Header closeButton >
                 <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}></Offcanvas.Title>
               </Offcanvas.Header>
-              <Offcanvas.Body>
+
+              <Offcanvas.Body >
                 <Nav className="justify-content-start  flex-grow-1" >
-                <NavLink to="/" className="text-3">inicio</NavLink>
-                <NavLink to="/RevistaDigitalPage" className="text-3">Revista Digital</NavLink>
-                <NavLink to="/HerramientasPage" className="text-3">Herramientas</NavLink>   
-                <NavLink to="/ObservatorioPage" className="text-3">Observatorio</NavLink>   
+                <NavLink to="/" onClick={closeOffCanvas}   className="text-3">inicio</NavLink>
+                <NavLink to="/RevistaDigitalPage" onClick={closeOffCanvas}  className="text-3">Revista Digital</NavLink>
+                <NavLink to="/HerramientasPage"onClick={closeOffCanvas} className="text-3">Herramientas</NavLink>   
+                <NavLink to="/ObservatorioPage" onClick={closeOffCanvas} className="text-3">Observatorio</NavLink>   
                 </Nav>
                 <Nav className="navbar-buttons">
                   
-                  <Button onClick={() => navigate("/LoginPage")}>iniciar sesión</Button>
-                  <Button onClick={() => navigate("/SingUpPage")}>registrarse</Button>
+                  <Button  onClick={() => {  navigate("/LoginPage")  ;  closeOffCanvas()  }} >iniciar sesión</Button>
+
+                  <Button onClick={() => {  navigate("/SingUpPage")  ;  closeOffCanvas()  }} >registrarse</Button>
             
                   <img src='assets/svgs/Escudo-UNSL-Blanco.svg'  width="100" height="50" className="d-inline-block " alt=""/>
                 </Nav>
