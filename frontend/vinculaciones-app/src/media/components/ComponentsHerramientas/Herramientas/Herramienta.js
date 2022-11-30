1bac18a1a5fcdb615831157f040f4052d2e9523e
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2022-11-30 06:53:10 -0300

    acomodando las rutas de las herramientas

--- frontend/vinculaciones-app/src/media/components/ComponentsHerramientas/Herramientas/Herramienta.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsHerramientas/Herramientas/Herramienta.js
@@ -4,21 +4,21 @@ import HerramientaCard from '../Herramientas/HerramientaCard';
 
 function herramienta(){
         const herramienta1={      
-                img:'Vinculaciones-Frontend/assets/images/herramienta.png',   
+                img:'assets/images/herramienta.png',   
                 hreft:'#0' ,
                 titulo:'Herramienta 1',                                    
                 texto:'Descripcion sobre la herramienta y su fin '   
         }
 
         const herramienta2={      
-                img:'Vinculaciones-Frontend/assets/images/herramienta.png',   
+                img:'assets/images/herramienta.png',   
                 hreft:'#0' ,
                 titulo:'Herramienta 2',                                    
                 texto:'Descripcion sobre la herramienta y su fin ' 
         }
 
          const herramienta3={      
-                img:'Vinculaciones-Frontend/assets/images/herramienta.png',   
+                img:'assets/images/herramienta.png',   
                 hreft:'#0',
                 titulo:'Herramienta 3',                                    
                 texto:'Descripcion sobre la herramienta y su fin',
