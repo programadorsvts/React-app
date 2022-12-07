0470f3314e1687316349d8e659cc921a8f855eb1
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2022-12-06 22:44:20 -0300

    Agregando mas herramientas

--- frontend/vinculaciones-app/src/media/components/ComponentsObi/Obi.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsObi/Obi.js
@@ -62,21 +62,122 @@ function herramienta(){
                 texto:'Desde la ciencia fundamental hasta la investigación nueva y novedosa, descubra nuestra gran colección de publicaciones sobre ciencias físicas e ingeniería, que abarcan una variedad de disciplinas, desde la teoría hasta la aplicación.',
                 parrafo:'',
         }
-         const herramienta9={      
+        const herramienta9={      
                 img:'assets/images/herramienta.png',   
                 href:'https://www.trademap.org/Index.aspx' ,
                 titulo:'Trade Map',                                    
                 texto:'Trade Map proporciona, en forma de tablas, gráficos y mapas, indicadores sobre el desempeño de las exportaciones, la demanda internacional, mercados alternativos y mercados competitivos, así como un directorio de empresas importadoras y exportadoras.',   
                 parrafo:'',
         }
+           const herramienta10={      
+                img:'assets/images/herramienta.png',   
+                href:'https://www.scopus.com/home.uri' ,
+                titulo:'Scopus',                                    
+                texto:'Scopus offers free features to non-subscribed users as Scopus Preview. Researchers use Scopus Preview to assist with research, such as searching for authors and learning more about Scopus content coverage and source metrics.',   
+                parrafo:'',
+        }
+           const herramienta11={      
+                img:'assets/images/herramienta.png',   
+                href:'https://www.rsc.org/' ,
+                titulo:'Royal Society of Chemistry',                                    
+                texto:'Whether studying the chemistry of life, or developing the advanced science behind modern technology, chemical scientists use their expertise to improve our health, our environment and our daily lives.',
+        }
+           const herramienta12={      
+                img:'assets/images/herramienta.png',   
+                href:'https://pubs.acs.org/' ,
+                titulo:'ACS Publications',                                    
+                texto:'ACS Publications’ commitment to publishing high-quality content continues to attract impactful research that addresses the world’s most important challenges.',   
+                parrafo:'',
+        }
+        const herramienta13={      
+                img:'assets/images/herramienta.png',   
+                href:'https://www.trademap.org/Index.aspx' ,
+                titulo:'Trade Map',                                    
+                texto:'Trade Map proporciona, en forma de tablas, gráficos y mapas, indicadores sobre el desempeño de las exportaciones, la demanda internacional, mercados alternativos y mercados competitivos, así como un directorio de empresas importadoras y exportadoras.',   
+                parrafo:'',
+        }
+           const herramienta14={      
+                img:'assets/images/herramienta.png',   
+                href:'https://www.springer.com/gp/' ,
+                titulo:'Springer',                                    
+                texto:'Springer is a leading global scientific, technical and medical portfolio, providing researchers in academia, scientific institutions and corporate R&D departments with quality content through innovative information, products and services.',   
+                parrafo:'',
+        }
+        const herramienta15={      
+                img:'assets/images/herramienta.png',   
+                href:'https://scholar.google.com/' ,
+                titulo:'Google Scholar',                                    
+                texto:'Google Scholar o Google Académico  (en Castellano) es el buscador de Google especializado en documentos académicos. Fué creado en 2004 y es el que debes usar si eres universitario o estás trabajando en contenidos académicos. En este buscador solo aparecen artículos publicados en revistas indexadas, tesis, libros, patentes y documentos relativos a congresos con validez científica y académica. Se trata pues de la mayor fuente de información apta para ser incluida como bibliografía en cualquier documento cuyas fuentes deban ser validas académicamente.',   
+                parrafo:'',
+        }
+        const herramienta16={      
+                img:'assets/images/herramienta.png',   
+                href:'https://scielo.org/es/' ,
+                titulo:'SciElO',                                    
+                texto:'SciELO Data ofrece a la comunidad de autores y editores de revistas de la Red SciELO y usuarios de SciELO Preprint un repositorio de datos de investigación que adopta  las mejores prácticas y estándares internacionales para la gestión de datos de investigación.',   
+                parrafo:'',
+        }
+        const herramienta17={      
+                img:'assets/images/herramienta.png',   
+                href:'https://pubmed.ncbi.nlm.nih.gov/' ,
+                titulo:'PubMed',                                    
+                texto:'PubMed is a free resource supporting the search and retrieval of biomedical and life sciences literature with the aim of improving health–both globally and personally.',   
+                parrafo:'',
+        }
+        const herramienta18={      
+                img:'assets/images/herramienta.png',   
+                href:'https://www.science.gov/',
+                titulo:'Science.gov',                                    
+                texto:'Science.gov searches over 60 databases and over 2,200 scientific websites to provide users with access to more than 200 million pages of authoritative federal science information including research and development results.',   
+               
+        }
+        const herramienta19={      
+                img:'assets/images/herramienta.png',   
+                href:'https://www.base-search.net/' ,
+                titulo:'BASE',                                    
+                texto:'BASE is one of the worlds most voluminous search engines especially for academic web resources. BASE provides more than 300 million documents from more than 10,000 content providers. You can access the full texts of about 60% of the indexed documents for free (Open Access). BASE is operated by Bielefeld University Library.',   
+           
+        }
+        const herramienta20={      
+                img:'assets/images/herramienta.png',   
+                href:'https://www.wolframalpha.com/' ,
+                titulo:'WolframAlpha',                                    
+                texto:'La introducción de Wolfram|Alpha marcó un paradigma fundamentalmente nuevo para la obtención de conocimiento y respuestas, no solo mediante búsquedas en la web, sino mediante cálculos dinámicos basados en una amplia colección de datos, algoritmos y métodos incorporados.Llevamos extenso y profundo conocimiento de nivel experto a cualquier persona... en cualquier momento y lugar.',   
+       
+        }
+          const herramienta21={      
+                img:'assets/images/herramienta.png',   
+                href:'https://www.hindawi.com/' ,
+                titulo:'Hindawi',                                    
+                texto:'One of the worlds largest fully open access journal publishers. We believe in openness, in scholarly publishing and research communication.',   
+        }
+
+          const herramienta22={      
+                img:'assets/images/herramienta.png',   
+                href:'https://clarivate.com/webofsciencegroup/solutions/web-of-science/' ,
+                titulo:'Clarivate',                                    
+                texto:'Clarivate Analytics es una empresa que posee y opera una colección de servicios enfocados principalmente al análisis de datos, incluyendo la investigación científica y académica de Google Analytics para patentes, patrones regulatorios, protección de marcas comerciales, inteligencia farmacéutica y de biotecnología, protección de marcas de dominio y gestión de propiedad intelectual.',   
+        }
+        const herramienta23={      
+                img:'assets/images/herramienta.png',   
+                href:'https://www.elsevier.com/' ,
+                titulo:'Elsevier',                                    
+                texto:'Elsevier partners with leading science organizations and Economist Impact for a global collaboration to understand the impact of the pandemic on confidence in research — and to identify areas for action to support researchers.',   
+        }
+         const herramienta24={      
+                img:'assets/images/herramienta.png',   
+                href:'https://onlinelibrary.wiley.com/' ,
+                titulo:'Wiley Online Library',                                    
+                texto:'Firmly established as the worlds leading society publisher, Wiley offers an exceptional portfolio of over 8 million articles from 1,600 journals. Half of which are published in partnership with prestigious international scholarly and professional societies. Our titles cover the full spectrum of life, health and physical sciences, social science and the humanities, and include many titles ranked at the forefront of their field. ',   
+        }
+
+
 
    
         return(
                 <>
 
                         <Container className='herramienta-container'>
-                            
-                                
                                 <Row className='mt-5'>
                                         <h1 className='encabezado-2 encabezado-1 text-center mt-5'>Unidad de Búsqueda Inteligente</h1>
                                         <p className="encabezado-3 mt-5 text-center">Desde la Secretaria de Vinculaciones Tecnológica y Social brindamos herramientas digitales e innovadoras para lograr y optimizar búsquedas inteligentes</p> 
@@ -90,8 +191,7 @@ function herramienta(){
                                                 <ObiCard data={herramienta3}></ObiCard> 
                                         </Col>
                                 </Row>
-                                <Row className=''>
-                                                         
+                                <Row className=''>   
                                         <Col className='mt-5 d-flex justify-content-center'>
                                                 <ObiCard data={herramienta4}></ObiCard> 
                                         </Col>
@@ -114,10 +214,66 @@ function herramienta(){
                                                 <ObiCard data={herramienta9}></ObiCard> 
                                         </Col>
                                 </Row>
-                                
-                                
-
-                        
+                                <Row className=''>
+                                                         
+                                        <Col className='mt-5 d-flex justify-content-center'>
+                                                <ObiCard data={herramienta10}></ObiCard> 
+                                        </Col>
+                                         <Col className='mt-5 d-flex justify-content-center'>
+                                                <ObiCard data={herramienta11}></ObiCard> 
+                                        </Col>
+                                         <Col className='mt-5 d-flex justify-content-center'>
+                                                <ObiCard data={herramienta12}></ObiCard> 
+                                        </Col>
+                                </Row>
+                                  <Row className=''>
+                                                         
+                                        <Col className='mt-5 d-flex justify-content-center'>
+                                                <ObiCard data={herramienta13}></ObiCard> 
+                                        </Col>
+                                         <Col className='mt-5 d-flex justify-content-center'>
+                                                <ObiCard data={herramienta14}></ObiCard> 
+                                        </Col>
+                                         <Col className='mt-5 d-flex justify-content-center'>
+                                                <ObiCard data={herramienta15}></ObiCard> 
+                                        </Col>
+                                </Row>
+                                <Row className=''>
+                                                         
+                                        <Col className='mt-5 d-flex justify-content-center'>
+                                                <ObiCard data={herramienta16}></ObiCard> 
+                                        </Col>
+                                         <Col className='mt-5 d-flex justify-content-center'>
+                                                <ObiCard data={herramienta17}></ObiCard> 
+                                        </Col>
+                                         <Col className='mt-5 d-flex justify-content-center'>
+                                                <ObiCard data={herramienta18}></ObiCard> 
+                                        </Col>
+                                </Row>
+                                <Row className=''>
+                                                         
+                                        <Col className='mt-5 d-flex justify-content-center'>
+                                                <ObiCard data={herramienta19}></ObiCard> 
+                                        </Col>
+                                         <Col className='mt-5 d-flex justify-content-center'>
+                                                <ObiCard data={herramienta20}></ObiCard> 
+                                        </Col>
+                                         <Col className='mt-5 d-flex justify-content-center'>
+                                                <ObiCard data={herramienta21}></ObiCard> 
+                                        </Col>
+                                </Row>
+                                <Row className=''>
+                                                         
+                                        <Col className='mt-5 d-flex justify-content-center'>
+                                                <ObiCard data={herramienta22}></ObiCard> 
+                                        </Col>
+                                         <Col className='mt-5 d-flex justify-content-center'>
+                                                <ObiCard data={herramienta23}></ObiCard> 
+                                        </Col>
+                                         <Col className='mt-5 d-flex justify-content-center'>
+                                                <ObiCard data={herramienta24}></ObiCard> 
+                                        </Col>
+                                </Row>
                         </Container>   
                 </>
         );
