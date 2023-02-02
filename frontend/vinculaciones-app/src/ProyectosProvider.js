b4036dd1a7adb87b5738e327b27c4fa4ff58e7dc
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2023-02-02 18:20:51 -0300

    Ordenando el Proyectos Provider

--- frontend/vinculaciones-app/src/ProyectosProvider.js
+++ frontend/vinculaciones-app/src/ProyectosProvider.js
@@ -3,7 +3,7 @@ import { useState,useContext} from "react";
 import Axios  from "axios";
 
 import Swal from 'sweetalert2'
-import { useNavigate } from 'react-router-dom';
+/* import { useNavigate } from 'react-router-dom'; */
 
 const MisProyectosContext =React.createContext();
 const ProyectosContext =React.createContext();
@@ -22,34 +22,31 @@ export const useSingProyectosPaginationContext=()=>{
 
 export function LoginProvider( {children } ){
           
-    const [auth, setAuth] = useState(null);
-    const [errorSubmit, setErrorSubmit] = useState("");
-    const navigate = useNavigate();
+    const [proyectos, setProyectos] = useState(null)
+/*     const navigate = useNavigate(); */
      
-     /////////////////User Auth/////////////////////////////////
-            const AuthUser= ()=> {
-                Axios.get('/sanctum/csrf-cookie' ).then(response => {
-                        Axios.get('http://127.0.0.1:8000/api/checkAuth' )
-                        .then((response) => {
-                            console.log(response)
-                            
-                            setAuth(true);
-                        })
-                        .catch((error)=> {
-                            console.log(error.messages);
-                            setAuth(false);
-                        })
-                    }); 
-                return auth;
+     ///////////////// Get Projects /////////////////////////////////
+            const Proyectos= ()=> {
+                Axios.get('http://127.0.0.1:8000/api/proyects')
+                .then(response => {
+                    setProyectos(response.data)
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    Swal.fire ({ icon: 'warning', title: 'Proyectos no encontrados',  timer: 2000 });
+                })
+                console.log('cartel dentro del get proyectos',proyectos)
+                return proyectos;
+      
             }       
      /////////////////////////////////////////////////////////////
       
     return(
-        <ProyectosContext.Provider value={}>
-            <MisProyectosContext.Provider value={}>
-                <SingProyectosPaginationContext.Provider value={}>
+        <ProyectosContext.Provider value={Proyectos}>
+            <MisProyectosContext.Provider value={Proyectos}>
+                <ProyectosPaginationContext.Provider>
                         {children}
-                </SingProyectosPaginationContext.Provider>
+                </ProyectosPaginationContext.Provider>
             </MisProyectosContext.Provider>    
         </ProyectosContext.Provider>
     );
