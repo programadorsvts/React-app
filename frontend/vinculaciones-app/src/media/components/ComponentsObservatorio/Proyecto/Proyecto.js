c479968377775eb3f7403dc566ca30cc50299ed8
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2023-02-16 01:33:32 -0300

    terminando el estilo de la paginacion , queda ver el estilo en vista de celular

--- frontend/vinculaciones-app/src/media/components/ComponentsObservatorio/Proyecto/Proyecto.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsObservatorio/Proyecto/Proyecto.js
@@ -20,14 +20,13 @@ function ProyectosPublicados () {
   
     useEffect(()=>{
          const getData = async () =>{
-                 await Axios.get('http://127.0.0.1:8000/api/proyects?to=10').then((response)=>{
+                 await Axios.get('http://127.0.0.1:8000/api/proyects?to=5').then((response)=>{
                    /*  http://127.0.0.1:8000/api/proyects?page=1&to=10 */
-                    setProyectos([...response.data.data])    
+                    setProyectos([...response.data.data])     
                 }) 
                 .catch((error)=> {
                     console.log(error.messages);
                 })     
-           
            setCurrentItems( proyectos.slice(itemOffset, endOffset));
            setPageCount( Math.ceil(proyectos.length / itemsPerPage));
          }
@@ -44,25 +43,21 @@ function ProyectosPublicados () {
             <Container>
                   <h1 className='encabezado-2 mb-5'>Catalogo de Proyectos de Inovacion</h1>
                   <Container className="proyecto">
-                        
-                        
                           {currentItems.map((currentItems) => {
                             
                               return(<ProyectoCard key={currentItems.id} proyecto={currentItems} ></ProyectoCard>)
                           })}  
                   </Container>
+                  <ReactPaginate  className='pagination d-flex justify-content-center mt-5 encabezado-4'
+                          breakLabel="..."
+                          nextLabel="siguiente >"
+                          onPageChange={handlePageClick}
+                          pageRangeDisplayed={5}
+                          pageCount={pageCount}
+                          previousLabel="< previo"
+                          renderOnZeroPageCount={null}
+                    />
             </Container>   
-                         
-                <ReactPaginate 
-                        className='pagination'
-                        breakLabel="..."
-                        nextLabel="siguiente >"
-                        onPageChange={handlePageClick}
-                        pageRangeDisplayed={5}
-                        pageCount={pageCount}
-                        previousLabel="< previo"
-                        renderOnZeroPageCount={null}
-                  />
         </>
     );
 
