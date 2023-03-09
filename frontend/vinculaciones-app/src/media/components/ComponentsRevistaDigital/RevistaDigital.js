46762b73b3223a1d40c889b03a71e77cc72bab94
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2023-03-09 16:44:21 -0300

    agregando los pdfs de las revistas

--- frontend/vinculaciones-app/src/media/components/ComponentsRevistaDigital/RevistaDigital.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsRevistaDigital/RevistaDigital.js
@@ -5,7 +5,7 @@ function revista(){
         const revista1=
         { 
                 imagen:'assets/revistadigital/revista1.jpg',   
-                link:'https://svts.unsl.edu.ar/pdf/UNSL2.0-Jun20.pdf ',                                   
+                link:'assets/revistadigital/revistapdf/Revista-1.pdf ',                                   
                 titulo:'Volumen 1 - Numero 1 ',    
                 fecha:'Junio 2020 ' , 
                 isbn:''  
@@ -13,7 +13,7 @@ function revista(){
         const revista2=
         { 
                 imagen:'assets/revistadigital/revista2.jpg',   
-                link:'https://svts.unsl.edu.ar/pdf/Sept20-Rev.N2-iUNSL2.0-3.pdf ',                 
+                link:'assets/revistadigital/revistapdf/Revista-2.pdf ',                 
                 titulo:'Volumen 1 - Numero 2',      
                 fecha:'septiembre 2020' , 
                 isbn:''  
@@ -21,7 +21,7 @@ function revista(){
         const revista3=
         { 
                 imagen:'assets/revistadigital/revista3.jpg',   
-                link:'https://svts.unsl.edu.ar/pdf/iunsl2.0-No3.-.pdf',                                      
+                link:'assets/revistadigital/revistapdf/Revista-3.pdf',                                      
                 titulo:'Volumen 1 - Numero 3',      
                 fecha:'diciembre 2020 ' , 
                 isbn:''  
@@ -29,7 +29,7 @@ function revista(){
         const revista4=
         { 
                 imagen:'assets/revistadigital/revista4.jpg',   
-                link:'https://svts.unsl.edu.ar/pdf/iUNSL2.0%20No4.%20-comprimido.pdf',    
+                link:'assets/revistadigital/revistapdf/Revista-4.pdf ',    
                 titulo:'Volumen 2 - Numero 4',      
                 fecha:'Junio 2021 ' , 
                 isbn:''  
@@ -37,7 +37,7 @@ function revista(){
         const revista5=
         {  
                 imagen:'assets/revistadigital/revista5.jpg',  
-                link:'https://svts.unsl.edu.ar/pdf/Iunsl%202.0%20No5.pdf',                            
+                link:'assets/revistadigital/revistapdf/Revista-5.pdf',                            
                 titulo:'Volumen 2 - Numero 5' ,    
                 fecha:'Diciembre 2021 ' , 
                 isbn:''  
