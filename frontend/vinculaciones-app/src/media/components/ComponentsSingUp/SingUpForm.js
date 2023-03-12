a984cdabd79ff2d35dbb6c4d582839f82fa0d308
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2023-03-12 05:24:07 -0300

    Cambiando el aviso del mail de la UNSL

--- frontend/vinculaciones-app/src/media/components/ComponentsSingUp/SingUpForm.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsSingUp/SingUpForm.js
@@ -37,7 +37,6 @@ const SingUpForm =() => {
                 <h1 className="encabezado-3 title">Registrarse</h1>
                 <Form.Group className="mb-3" controlId="CorreoSingUp" >
                   <Form.Label className="encabezado-4 label" >Correo Electrónico:</Form.Label>
-                  <p className="text-5 label-secundary">debe ser de formato unsl.edu.ar o email.unsl.edu.ar</p>
                   <Form.Control  
                       type="email"
                       name='email'
