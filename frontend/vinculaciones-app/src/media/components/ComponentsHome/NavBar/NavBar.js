65cf23b545b2d5bce69bdd590675b9b52798677c
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2023-03-19 16:32:44 -0300

    mejorando el cierre del menu offcanvas , el anterior tiraba un error

--- frontend/vinculaciones-app/src/media/components/ComponentsHome/NavBar/NavBar.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsHome/NavBar/NavBar.js
@@ -22,8 +22,14 @@ function NavBar() {
           setAuth(AuthUser()); 
   }, [AuthUser])
 
-  const offCanvasRef = useRef(true);
-  const closeOffCanvas = () => offCanvasRef.current.backdrop.click() ; 
+  const [show, setShow] = useState(true);
+
+  const closeOffCanvas = () => {
+    setShow((show) => !show);
+  };
+  const openToogle=()=>{
+    setShow(true)
+  }
   
 
   
@@ -44,7 +50,36 @@ function NavBar() {
 
   return (
     <>
-      {['lg' ].map((expand) => (
+        <Navbar expand="lg" className="navbar" sticky="top"  >
+              <Container fluid>
+                     <NavLink to="/" id="navlogo" >
+                      <img src='assets/svgs/Solo-LogoSVTS.svg' width="100" height="50" className="d-inline-block " alt="logo1" id="imglogo"/>
+                    </ NavLink >
+                <Navbar.Toggle  className='navbar-toggler' aria-controls="offcanvasNavbar"  onClick={openToogle}/>
+                <Navbar.Collapse id="basic-navbar-nav" >
+                   <Navbar.Offcanvas className='menu-toggle' placement="end" restoreFocus={false} show={show} onHide={closeOffCanvas}>
+                      <Offcanvas.Header closeButton >
+                        <Offcanvas.Title>Tocar fuera del menu para volver</Offcanvas.Title>
+                      </Offcanvas.Header>
+                      <Offcanvas.Body onClick={closeOffCanvas}>
+                        <Nav className="justify-content-start flex-grow-1"  id="navbtns">
+                          <NavLink to="/" className="text-3" onClick={closeOffCanvas} id="inicio" >inicio</NavLink>
+                          <NavLink to="/RevistaDigitalPage" onClick={closeOffCanvas} className="text-3" id="revista">Revista Digital</NavLink>
+                          <NavLink to="/ObiPage"  onClick={closeOffCanvas} className="text-3" id="obi">Ubi </NavLink>  
+                          <NavLink to="/ObservatorioPage"  onClick={closeOffCanvas}  className="text-3" id="observatorio">Observatorio</NavLink>   
+                        </Nav>
+                        <Nav className="navbar-buttons" id="btnuser" >
+                          { UserButtons}
+                          <img src='assets/svgs/Escudo-UNSL-Blanco.svg'  width="100" height="50" className="d-inline-block " alt=""/>
+                        </Nav>
+                      </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
+
+
+     {/*  {['lg' ].map((expand) => (
         
         <Navbar id="navbar" key={expand} expand={expand} className="navbar" sticky="top" >
           <Container fluid id="container" >
@@ -72,7 +107,7 @@ function NavBar() {
             </Navbar.Offcanvas>
           </Container>
         </Navbar>
-      ))}
+      ))} */}
     </>
   );
 }
