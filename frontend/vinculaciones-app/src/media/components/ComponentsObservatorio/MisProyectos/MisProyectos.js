d533ec225846c11e1ecc9bbd33e971c28e135c4c
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2023-03-22 16:22:08 -0300

    cambiando la ruta de mis proyectos

--- frontend/vinculaciones-app/src/media/components/ComponentsObservatorio/MisProyectos/MisProyectos.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsObservatorio/MisProyectos/MisProyectos.js
@@ -10,57 +10,29 @@ import { API_URL } from '../../../../config/env';
 function MisProyectos () {
 
   const [proyectos, setProyectos] = useState([]); //Proyectos del backend variable llamada 'proyectos'
-  const [prevUrl, setPrevUrl] = useState(""); //Proyectos del backend variable llamada 'proyectos'
-  const [nextUrl, setNextUrl] = useState(""); //Proyectos del backend variable llamada 'proyectos'
+
 
   useEffect(() => {
-    Axios.get(API_URL + "/api/proyects?page=1&to=20")
+    Axios.get(API_URL + "api/user/proyects")
       .then((response) => {
         setProyectos(response.data.data);
-        setPrevUrl(response.data.prev_page_url);
-        setNextUrl(response.data.next_page_url);
       })
       .catch((error) => {
         console.log(error.response.data);
       });
   }, []);
-
-  const nextPage = (url) => {
-    Axios.get(url + "&to=20").then((response) => {
-      setProyectos((state) => response.data.data);
-      setPrevUrl(response.data.prev_page_url);
-      setNextUrl(response.data.next_page_url);
-    });
-  };
-  const prevPage = (url) => {
-    Axios.get(url + "&to=20").then((response) => {
-      setProyectos((state) => response.data.data);
-      setPrevUrl(response.data.prev_page_url);
-      setNextUrl(response.data.next_page_url);
-    });
   };
   
   return (
     <>
-      <Container>
         <Container className="proyecto">
           {proyectos.map((item) => {
             return <ProyectoCard key={item.id} proyecto={item}></ProyectoCard>;
           })}
         </Container>
-        {prevUrl ? (
-          <button onClick={() => prevPage(prevUrl)}>Volver</button>
-        ) : (
-          ""
-        )}
-        {nextUrl ? (
-          <button onClick={() => nextPage(nextUrl)}>Siguiente</button>
-        ) : (
-          ""
-        )}
-      </Container>
+
     </>
   );
-}
+
 
  export default MisProyectos
