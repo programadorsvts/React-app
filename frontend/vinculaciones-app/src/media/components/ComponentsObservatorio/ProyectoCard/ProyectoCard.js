64fb77b914d117c9fbfe9a8eba886c06d73c45f6
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2023-03-28 23:05:03 -0300

    Update ProyectoCard.js

--- frontend/vinculaciones-app/src/media/components/ComponentsObservatorio/ProyectoCard/ProyectoCard.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsObservatorio/ProyectoCard/ProyectoCard.js
@@ -3,34 +3,28 @@ import "./proyectocard.css";
 import { useState } from "react";
 
 import ProyectoModal from "../ProyectoCard/ProyectoModal";
- const [areas, setAreas] = useState([]);
-        
+
 function ProyectoCard({ proyecto }) {
   const [modalShow, setModalShow] = useState(false);
-  useEffect(() => {
-    Axios.get(API_URL + "api/area")
-      .then((response) => {
-        setAreas(response.data);
-      })
-      .catch((err) => console.error(err));
-    
+  const [areas, setAreas] = useState([]);
+        
   return (
     <>
-      <Card className="card" onClick={() => setModalShow(true)}>
-        <Card.Body>
-          <Card.Text className="encabezado-4 text-lowercase">
-            {proyecto.title}
-          </Card.Text>
-          <Card.Title className="text-1 text-deg text-capitalize">
-           {/*  {proyecto.area.name} */}
-          </Card.Title>
-        </Card.Body>
-      </Card>
-      <ProyectoModal
-        proyecto={proyecto}
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-      />
+        <Card className="card" onClick={() => setModalShow(true)}>
+          <Card.Body>
+            <Card.Text className="encabezado-4 text-lowercase">
+              {proyecto.title}
+            </Card.Text>
+            <Card.Title className="text-1 text-deg text-capitalize">
+            {/*  {proyecto.area.name} */}
+            </Card.Title>
+          </Card.Body>
+        </Card>
+        <ProyectoModal
+          proyecto={proyecto}
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+        />
     </>
   );
 }
