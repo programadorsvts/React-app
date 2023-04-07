47586925923400b3bbc7be44014ff137020ba11f
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2023-04-07 15:26:58 -0300

    Cambiando el formato de las revistas a Webp

--- frontend/vinculaciones-app/src/media/components/ComponentsRevistaDigital/RevistaDigital.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsRevistaDigital/RevistaDigital.js
@@ -1,99 +1,99 @@
-import {Row,Container, Col} from 'react-bootstrap';
+import { Row, Container, Col } from 'react-bootstrap';
 import RevistaCard from '../ComponentsRevistaDigital/RevistaCard';
 
-function revista(){
-        const revista1=
-        { 
-                imagen:'assets/images/revista1.jpg',   
-                link:'assets/revistadigital/revistapdf/Revista-1.pdf ',                                   
-                titulo:'Volumen 1 - Numero 1 ',    
-                fecha:'Junio 2020 ' , 
-                isbn:''  
+function revista() {
+        const revista1 =
+        {
+                imagen: 'assets/images/revista1.webp',
+                link: 'assets/revistadigital/revistapdf/Revista-1.pdf ',
+                titulo: 'Volumen 1 - Numero 1 ',
+                fecha: 'Junio 2020 ',
+                isbn: ''
         }
-        const revista2=
-        { 
-                imagen:'assets/images/revista2.jpg',   
-                link:'assets/revistadigital/revistapdf/Revista-2.pdf ',                 
-                titulo:'Volumen 1 - Numero 2',      
-                fecha:'septiembre 2020' , 
-                isbn:''  
+        const revista2 =
+        {
+                imagen: 'assets/images/revista2.webp',
+                link: 'assets/revistadigital/revistapdf/Revista-2.pdf ',
+                titulo: 'Volumen 1 - Numero 2',
+                fecha: 'septiembre 2020',
+                isbn: ''
         }
-        const revista3=
-        { 
-                imagen:'assets/images/revista3.jpg',   
-                link:'assets/revistadigital/revistapdf/Revista-3.pdf',                                      
-                titulo:'Volumen 1 - Numero 3',      
-                fecha:'diciembre 2020 ' , 
-                isbn:''  
+        const revista3 =
+        {
+                imagen: 'assets/images/revista3.webp',
+                link: 'assets/revistadigital/revistapdf/Revista-3.pdf',
+                titulo: 'Volumen 1 - Numero 3',
+                fecha: 'diciembre 2020 ',
+                isbn: ''
         }
-        const revista4=
-        { 
-                imagen:'assets/images/revista4.jpeg',   
-                link:'assets/revistadigital/revistapdf/Revista-4.pdf ',    
-                titulo:'Volumen 2 - Numero 4',      
-                fecha:'Junio 2021 ' , 
-                isbn:''  
+        const revista4 =
+        {
+                imagen: 'assets/images/revista4.webp',
+                link: 'assets/revistadigital/revistapdf/Revista-4.pdf ',
+                titulo: 'Volumen 2 - Numero 4',
+                fecha: 'Junio 2021 ',
+                isbn: ''
         }
-        const revista5=
-        {  
-                imagen:'assets/images/revista5.jpeg',  
-                link:'assets/revistadigital/revistapdf/Revista-5.pdf',                            
-                titulo:'Volumen 2 - Numero 5' ,    
-                fecha:'Diciembre 2021 ' , 
-                isbn:''  
+        const revista5 =
+        {
+                imagen: 'assets/images/revista5.webp',
+                link: 'assets/revistadigital/revistapdf/Revista-5.pdf',
+                titulo: 'Volumen 2 - Numero 5',
+                fecha: 'Diciembre 2021 ',
+                isbn: ''
         }
-          const revista6=
-        {  
-                imagen:'assets/images/revista6.jpg',  
-                link:'assets/revistadigital/revistapdf/Revista-6.pdf',                            
-                titulo:'Volumen 3 - Numero 6' ,    
-                fecha:'Diciembre 2022 ' , 
-                isbn:''  
+        const revista6 =
+        {
+                imagen: 'assets/images/revista6.webp',
+                link: 'assets/revistadigital/revistapdf/Revista-6.pdf',
+                titulo: 'Volumen 3 - Numero 6',
+                fecha: 'Diciembre 2022 ',
+                isbn: ''
         }
-        
-        return(
+
+        return (
                 <>
                         <Container>
-                            <h1 className='encabezado-2 encabezado-1 text-center mt-5'>Publicaciones</h1>
-                            <Row   className='mt-5'>
-                                  <Col className='mt-5 d-flex justify-content-center'>
+                                <h1 className='encabezado-2 encabezado-1 text-center mt-5'>Publicaciones</h1>
+                                <Row className='mt-5'>
+                                        <Col className='mt-5 d-flex justify-content-center'>
 
-                                        <RevistaCard data={revista6}></RevistaCard>
+                                                <RevistaCard data={revista6}></RevistaCard>
 
-                                </Col>
-                                <Col className='mt-5 d-flex justify-content-center'>
+                                        </Col>
+                                        <Col className='mt-5 d-flex justify-content-center'>
 
-                                        <RevistaCard data={revista5}></RevistaCard>
+                                                <RevistaCard data={revista5}></RevistaCard>
 
-                                </Col>
-                                <Col className='mt-5  d-flex justify-content-center'>
+                                        </Col>
+                                        <Col className='mt-5  d-flex justify-content-center'>
 
-                                        <RevistaCard data={revista4}></RevistaCard>
+                                                <RevistaCard data={revista4}></RevistaCard>
 
-                                </Col>
-                            </Row>
+                                        </Col>
+                                </Row>
 
-                             <Row   className='mt-5'>
-                                 <Col className='mt-5  d-flex justify-content-center'>
+                                <Row className='mt-5'>
+                                        <Col className='mt-5  d-flex justify-content-center'>
 
-                                        <RevistaCard data={revista3}></RevistaCard>
+                                                <RevistaCard data={revista3}></RevistaCard>
 
-                                </Col>
-                                <Col className='mt-5 d-flex justify-content-center'>
+                                        </Col>
+                                        <Col className='mt-5 d-flex justify-content-center'>
 
-                                        <RevistaCard data={revista2}></RevistaCard>
+                                                <RevistaCard data={revista2}></RevistaCard>
 
-                                </Col>
-                                <Col className='mt-5  d-flex justify-content-center'>
+                                        </Col>
+                                        <Col className='mt-5  d-flex justify-content-center'>
 
-                                        <RevistaCard data={revista1}></RevistaCard>
+                                                <RevistaCard data={revista1}></RevistaCard>
 
-                                </Col>
-                            </Row>
-                        </Container>   
+                                        </Col>
+                                </Row>
+                        </Container>
                 </>
         );
 }
 
 
- export default revista;
+export default revista;
