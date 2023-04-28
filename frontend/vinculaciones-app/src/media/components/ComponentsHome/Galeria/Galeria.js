295de472a6c86edbc8d7fe6c5de9a282b778c07a
Octavio Riccardo <66325928+Batvizz@users.noreply.github.com>
2023-04-28 12:15:33 -0300

    Actualizando la Galeria principal

--- frontend/vinculaciones-app/src/media/components/ComponentsHome/Galeria/Galeria.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsHome/Galeria/Galeria.js
@@ -6,7 +6,7 @@ function galeria() {
   return (
     <Container className='galeria mt-5 mb-5'>
       <img src='assets/images/galeria-1.webp' alt='imagen-1'></img>
-      <img src='assets/images/galeria-2.webp' alt='imagen-2'></img>
+      <img className='h-50' src='assets/images/galeria-2.webp' alt='imagen-2'></img>
       <img src='assets/images/galeria-3.webp' alt='imagen-3'></img>
       <img src='assets/images/galeria-4.webp' alt='imagen-4'></img>
       <img src='assets/images/galeria-5.webp' alt='imagen-5'></img>
