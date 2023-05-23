8cc47b491f17ad3171dc1dfa7e729099d523efbd
MarcosGero <marcos.funcky@gmail.com>
2023-05-23 16:54:42 -0300

    Summary(required)
    Summary(required)

--- frontend/vinculaciones-app/src/media/components/ComponentsHome/Galeria/Galeria.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsHome/Galeria/Galeria.js
@@ -7,9 +7,10 @@ function Galeria() {
   const [modalShow, setModalShow] = useState(false);
   return (
     <Container className='galeria mt-5 mb-5'>
-      <img src='assets/images/galeria-1.webp' alt='imagen-1'></img>
+      <img className='card-element'src='assets/images/galeria-1.webp' alt='imagen-1' onClick={() => setModalShow(true)}></img>
       <img src='assets/images/galeria-2.jpg' alt='imagen-2'></img>
       <GaleriaModal show={modalShow} onHide={() => setModalShow(false)} />
+      <img src='assets/images/galeria-3.webp' alt='imagen-2'></img>
       <img src='assets/images/galeria-4.webp' alt='imagen-4'></img>
       <img src='assets/images/galeria-5.webp' alt='imagen-5'></img>
       <img src='assets/images/galeria-6.jpg' alt='imagen-6'></img>
--- frontend/vinculaciones-app/src/media/components/ComponentsHome/Galeria/GaleriaModal.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsHome/Galeria/GaleriaModal.js
@@ -2,6 +2,7 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 import './modal.css';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function galeriamodal(props) {
 
@@ -9,14 +10,37 @@ function galeriamodal(props) {
         <>
             <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
                 <Modal.Header closeButton>
-                    <Modal.Title className="text-1 text-deg text-capitalize">{'Titulo del Modal galeria '}</Modal.Title>
+                    <Modal.Title className="text-1 text-deg text-capitalize">{'EVENTOS '}</Modal.Title>
                 </Modal.Header>
-                <Modal.Body className='text-description'>
+                <Modal.Body className='text-description' >
+                    <h4 className='encabezado-2 mb-3 text-center'>{'props.proyecto.title'}</h4>
+                    <p className='text-1 mb-1  '>Organización {'prop'}</p>
+                    <p className='text-2'>Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                     ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+                     in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+                    
+                    <img className='imgModal1 mx-auto' src='assets/images/galeria-3.webp' alt='imagen-3'></img>
+                    <p className='text-1 mb-1 '>Fecha de eventos:</p>
+                    <Container >
+                        <Row>
 
-                    <img src='assets/images/galeria-3.webp' alt='imagen-3'></img>
+                            <Col className='col-6' >
+                                <p className='text-2 bg-danger'>Fecha 1</p>
+                            </Col>
 
+                            <Col className='col-4'>
+                                <p className='text-2 bg-warning'>Fecha 2</p>
+                            </Col>
+                            <Col className='col-2'>
+                                <p className='text-2 bg-success'>Fecha 2</p>
+                            </Col>
+                        </Row>
+                    </Container>
                 </Modal.Body>
                 <Modal.Footer>
+                    <Button onClick={props.onHide} >Ingresar</Button>
                     <Button onClick={props.onHide} >Cerrar</Button>
                 </Modal.Footer>
             </Modal>
