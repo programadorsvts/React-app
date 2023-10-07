405298f012497af7bcdcbf6f3148ad58ba65c8bb
MarcosGero <marcos.funcky@gmail.com>
2023-10-07 02:58:00 -0300

    realmente

---
+++ frontend/vinculaciones-app/src/media/pages/Banner.js
@@ -0,0 +1,12 @@
+import Image from 'react-bootstrap/Image';
+
+function Banner(){
+
+    return <>
+    <Image href={''} src='assets/images/banner.png' alt='imagen-4' fluid/>
+    </>;
+
+
+}
+
+export default Banner;
