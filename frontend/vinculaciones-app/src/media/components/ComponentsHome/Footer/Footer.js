52e42380465dd7db8c760327d3bddec29937ba94
MarcosGero <marcos.funcky@gmail.com>
2023-10-08 18:42:37 -0300

    .

--- frontend/vinculaciones-app/src/media/components/ComponentsHome/Footer/Footer.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsHome/Footer/Footer.js
@@ -12,7 +12,7 @@ function footer() {
                 <div className="text-center" xs={12} md={4}>
                     <h6 className="text-1">Información Institucional</h6>
                     <img src="assets/svgs/Escudo-UNSL-Blanco.svg" width="150" height="100" alt="" />
-                    <p className='text-4'>Dirección: <a className="address" href="https://www.google.com/maps/place/Universidad+Nacional+de+San+Luis/@-33.2920509,-66.341365,17.6z/data=!4m9!1m2!2m1!1sunsl!3m5!1s0x95d4394eec782487:0x2bcfdfba83514ad4!8m2!3d-33.2921332!4d-66.3396195!15sCgR1bnNsWgYiBHVuc2ySAQp1bml2ZXJzaXR5mgEkQ2hkRFNVaE5NRzluUzBWSlEwRm5TVVJWT1daNk5ucFJSUkFC4AEA" target="_blank" rel="noopener noreferrer">Ejército de los Andes 950<br></br> San Luis, Argentina</a></p>
+                    <p className='text-4'>Dirección: <a className="address" href="https://www.google.com/maps/place/Universidad+Nacional+de+San+Luis/@-33.2920509,-66.341365,17.6z/data=!4m9!1m2!2m1!1sunsl!3m5!1s0x95d4394eec782487:0x2bcfdfba83514ad4!8m2!3d-33.2921332!4d-66.3396195!15sCgR1bnNsWgYiBHVuc2ySAQp1bml2ZXJzaXR5mgEkQ2hkRFNVaE5NRzluUzBWSlEwRm5TVVJWT1daNk5ucFJSUkFC4AEA" target="_blank" rel="noopener noreferrer">Avda Ejército de los Andes 950<br></br> San Luis, Argentina</a></p>
                     <p className='text-4'>Telefono: +54 (0266) 4520300</p>
                 </div>
                 <div className="text-center" xs={12} md={4}>
@@ -21,7 +21,7 @@ function footer() {
                     <p>Ubicación: 1er Piso - Ala B</p>
                     <p>Telefono: +54 (0266) 4520300</p>
                     <p>Interno: 5159</p>
-                    <p>Email: svtsInterno@gmail.com</p>
+                    <p>Email: svts.unsl@gmail.com</p>
                 </div>
                 <div className="text-center" xs={12} md={4}>
                     <h6 className="text-1">Redes</h6>
--- frontend/vinculaciones-app/src/media/pages/Banner.js
+++ frontend/vinculaciones-app/src/media/pages/Banner.js
@@ -1,12 +1,18 @@
-import Image from 'react-bootstrap/Image';
-
-function Banner(){
-
-    return <>
-    <Image href={''} src='assets/images/banner.png' alt='imagen-4' fluid/>
-    </>;
-
-
+import Image from "react-bootstrap/Image";
+import { Container } from "react-bootstrap";
+function Banner() {
+  return (
+    <Container fluid className="text-center">
+      <a href={"assets/revistadigital/revistapdf/cartilla-prensa-workshop.pdf"} target="_blank" rel="noreferrer">
+        <Image
+          src="assets/images/banner.png"
+          alt="imagen-4"
+          fluid
+          className="banner-image"
+        />
+      </a>
+    </Container>
+  );
 }
 
 export default Banner;
