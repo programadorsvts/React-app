849e61396cf640564e9944eecf06fe0932de824a
MarcosGero <marcos.funcky@gmail.com>
2023-10-08 19:17:39 -0300

    Revert "Merge branch 'Marcos'"
    This reverts commit 2e04f09e48b9934bb034ef79799fab3e2a96a94f, reversing
    changes made to 2f8a549d4dedd08c5767f871219803eea5c1f59c.

--- frontend/vinculaciones-app/src/media/components/ComponentsObservatorio/CrearProyecto/CrearProyecto.js
+++ frontend/vinculaciones-app/src/media/components/ComponentsObservatorio/CrearProyecto/CrearProyecto.js
@@ -1,20 +1,27 @@
-import { useNavigate } from "react-router-dom";
-import { Button, Container } from "react-bootstrap";
-import { useEffect, useState } from "react";
-import { useAuthUserContext } from "../../../../LoginProvider";
+import {useNavigate } from "react-router-dom";
+import {Button,Container} from 'react-bootstrap';
+import { useEffect,useState } from "react";
+import  {useAuthUserContext}  from '../../../../LoginProvider';
 
 function CrearProyecto() {
+  
   const navigate = useNavigate();
+  const AuthUser=useAuthUserContext();
+  const [auth,setAuth]=useState(null);
 
+  useEffect(() => {
+    setAuth(AuthUser());
+  }, [AuthUser])
+  
   return (
+
     <Container className="d-flex justify-content-center mt-5">
-      <Button
-        className="btn btn-deg w-75"
-        onClick={() => navigate("/CrearProyectoPage")}
-      >
-        Crear nuevo proyecto
-      </Button>
-      <span></span>
+      {
+        auth ?
+          <Button className='btn btn-deg w-75' onClick={() => navigate("/CrearProyectoPage")}>Crear nuevo proyecto</Button>
+        : <span></span>
+        }
+ 
     </Container>
   );
 }
