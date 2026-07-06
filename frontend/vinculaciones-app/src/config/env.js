69bfa0e5da4875b232c6fd16944d1d32db66fc37
MarcosGero <marcos.funcky@gmail.com>
2026-07-06 08:05:33 -0300

    Update env.js

--- frontend/vinculaciones-app/src/config/env.js
+++ frontend/vinculaciones-app/src/config/env.js
@@ -1,2 +1,3 @@
 //export const API_URL = "http://127.0.0.1:8000/"
-export const API_URL = "https://laravel-lfq5j.ondigitalocean.app/"
+//export const API_URL = "https://laravel-lfq5j.ondigitalocean.app/"
+export const API_URL = "https://api-sbvt.unsl.edu.ar/"
